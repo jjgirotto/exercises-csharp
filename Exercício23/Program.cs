bf113187c33428671603712b2e99a1e491eb28d2
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-05-29 21:21:24 -0300

    Exercício 23 de lista encadeada

---
+++ Exercício23/Program.cs
@@ -0,0 +1,52 @@
+﻿tpno inicio = null;
+tpno fim = null;
+
+void Insere(int valor)
+{
+    tpno no = new tpno();
+    no.valor = valor;
+    if (inicio == null)
+    {
+        inicio = no;
+    }
+    else
+    {
+        fim.prox = no;
+    }
+    fim = no;
+}
+
+tpno Remove()
+{
+    tpno no = inicio;   
+    if (inicio != null)
+    {
+        inicio = inicio.prox;
+    }
+    if (inicio == null)
+    {
+        fim = null;
+    }
+    return no;
+}
+
+Console.Write("Quantos números deseja digitar? ");
+int qtde = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < qtde; i++)
+{
+    Console.Write("Digite o valor desejado: ");
+    int valor = Convert.ToInt32(Console.ReadLine());
+    Insere(valor);
+}
+Console.WriteLine("Números da lista: ");
+while (inicio != null)
+{
+    int numero = Remove().valor;
+    Console.WriteLine(numero);
+}
+Console.WriteLine(fim.valor);
+class tpno
+{
+    public int valor;
+    public tpno prox;
+}
